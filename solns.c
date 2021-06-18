05a777f87392256fea88fd9dd2f87021bf416b04
SHAHANAS P <52955084+shahanasp@users.noreply.github.com>
2021-06-18 18:46:02 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -6,7 +6,19 @@ int max(int arr[],int n)
     for (i = 0; i < n; i++) {
         if (arr[i]>=large){
             large=arr[i];
-        
+        }
+        else{
+            continue;
+        }
+    return (large);
+}
+int min(int arr[],int n)
+{
+    int i,small=0;
+    small=arr[0];
+    for (i = 0; i < n; i++) {
+        if (arr[i]<=large){
+            small=arr[i];
         }
         else{
             continue;
