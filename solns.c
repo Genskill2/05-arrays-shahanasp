29dff00af5c934245dc3f5d68f7a01b9fba78806
SHAHANAS P <52955084+shahanasp@users.noreply.github.com>
2021-06-18 18:41:27 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -4,7 +4,7 @@ int max(int arr[],int n)
     int i,large=0;
     large=arr[0];
     for (i = 0; i < n; i++) {
-        if (arr[i])>=large){
+        if (arr[i]>=large){
             large=arr[i];
         
         }
