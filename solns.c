ab2fbdea6bc237a5cad3e9c0e4803393a3a815b1
SHAHANAS P <52955084+shahanasp@users.noreply.github.com>
2021-06-18 13:02:03 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include <stdio.h>
+int max(int arr[],int n)
+{
+    for (i = 1; i < n; ++i) {
+        if (arr[0] < arr[i])
+            arr[0] = arr[i];
+    }
+
+    return (arr[0]);
+}
